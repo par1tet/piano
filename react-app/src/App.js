2d8f7ed444342bf3fbde1a6664b893d10c5eb855
par1tet <mankova486@gmail.com>
2024-06-18 18:12:26 +0300

    osci

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -57,7 +57,7 @@ function App() {
 		['G7',3136, 'nt'],
 	])
 
-	let [blackKeysNote, setBlackKeysNote] = useState([
+	const [blackKeysNote, setBlackKeysNote] = useState([
 		['A#0',29.14, 'Q', ''],
 		['',null, '', 'hide'],
 		['C#1',34.65, 'W', ''],
@@ -107,6 +107,18 @@ function App() {
 		['F1',43.654, '6', ''],
 		['',48.999, '', 'hide'],
 	])
+
+	const [attack, setAttack] = useState(1)
+	const [decay, setDecay] = useState(1600)
+	const [susteinLevel, setSusteinLevel] = useState(400)
+	const [susteinTime, setSusteinTime] = useState(3000)
+	const [relay, setRelay] = useState(240)
+	const [bias1, setBias1] = useState(0)
+	const [bias2, setBias2] = useState(0)
+	const [bias3, setBias3] = useState(0)
+	const [bias4, setBias4] = useState(0)
+	const [bias5, setBias5] = useState(0)
+	const [bias6, setBias6] = useState(0)
 
 	function play(ev, index){
 		let audioContext = new AudioContext();
@@ -186,37 +198,160 @@ function App() {
 		let volume = audioContext.createGain();
 		let pan = audioContext.createStereoPanner();
 	
-		volume.gain.value = 1;
+		volume.gain.value = 0;
 		pan.pan.value = 0;
 	
 		output.connect(volume);
 		volume.connect(audioContext.destination);
-
-		let oscillator = audioContext.createOscillator();
+	{
+		let oscillator1 = audioContext.createOscillator();
 
 		// oscillator.frequency.value = notes[index][1];f
 
-		let steps = +(document.querySelector('.sentezator > .settings > .set-steps > input').value);
+		// let steps = +(document.querySelector('.sentezator > .settings > .set-steps > input').value);
 
-		let imag = new global.Float32Array(steps);
-		let real = new global.Float32Array(steps);
+		// let imag = new global.Float32Array(steps);
+		// let real = new global.Float32Array(steps);
 		
-		for (var i = 1; i < steps; i++) {
-			imag[i] = 1 / (i * Math.PI);
-		}
+		// for (var i = 1; i < steps; i++) {
+		// 	imag[i] = 1 / (i * Math.PI);
+		// }
+
+		oscillator1.type = 'sawtooth'
+
+		// const wave = audioContext.createPeriodicWave(real, imag);
+
+		// oscillator.setPeriodicWave(wave);
+		oscillator1.frequency.setValueAtTime(((keys[index][1] + +(document.querySelectorAll('.set-osc')[0].children[2].value))), audioContext.currentTime);
+		oscillator1.connect(output);
+		oscillator1.start();
+	}
+
+	{
+		let oscillator2 = audioContext.createOscillator();
+
+		// oscillator.frequency.value = notes[index][1];f
+
+		// let steps = +(document.querySelector('.sentezator > .settings > .set-steps > input').value);
+
+		// let imag = new global.Float32Array(steps);
+		// let real = new global.Float32Array(steps);
 		
-		const wave = audioContext.createPeriodicWave(real, imag);
+		// for (var i = 1; i < steps; i++) {
+		// 	imag[i] = 1 / (i * Math.PI);
+		// }
 
-		oscillator.setPeriodicWave(wave);
+		oscillator2.type = 'sawtooth'
 
-		oscillator.frequency.setValueAtTime(keys[index][1], audioContext.currentTime);
-		oscillator.connect(output);
-		oscillator.start();
+		// const wave = audioContext.createPeriodicWave(real, imag);
+
+		// oscillator.setPeriodicWave(wave);
+		oscillator2.frequency.setValueAtTime(((keys[index][1] + +(document.querySelectorAll('.set-osc')[1].children[2].value))), audioContext.currentTime);
+		oscillator2.connect(output);
+		oscillator2.start();
+	}
+
+	{
+		let oscillator3 = audioContext.createOscillator();
+
+		// oscillator.frequency.value = notes[index][1];f
+
+		// let steps = +(document.querySelector('.sentezator > .settings > .set-steps > input').value);
+
+		// let imag = new global.Float32Array(steps);
+		// let real = new global.Float32Array(steps);
+		
+		// for (var i = 1; i < steps; i++) {
+		// 	imag[i] = 1 / (i * Math.PI);
+		// }
+
+		oscillator3.type = 'sawtooth'
+
+		// const wave = audioContext.createPeriodicWave(real, imag);
+
+		// oscillator.setPeriodicWave(wave);
+		oscillator3.frequency.setValueAtTime(((keys[index][1] + +(document.querySelectorAll('.set-osc')[2].children[2].value))), audioContext.currentTime);
+		oscillator3.connect(output);
+		oscillator3.start();
+	}
+
+	{
+		let oscillator4 = audioContext.createOscillator();
+
+		// oscillator.frequency.value = notes[index][1];f
+
+		// let steps = +(document.querySelector('.sentezator > .settings > .set-steps > input').value);
+
+		// let imag = new global.Float32Array(steps);
+		// let real = new global.Float32Array(steps);
+		
+		// for (var i = 1; i < steps; i++) {
+		// 	imag[i] = 1 / (i * Math.PI);
+		// }
+
+		oscillator4.type = 'sawtooth'
+
+		// const wave = audioContext.createPeriodicWave(real, imag);
+
+		// oscillator.setPeriodicWave(wave);
+		oscillator4.frequency.setValueAtTime(((keys[index][1] + +(document.querySelectorAll('.set-osc')[3].children[2].value))), audioContext.currentTime);
+		oscillator4.connect(output);
+		oscillator4.start();
+	}
+
+	{
+		let oscillator5 = audioContext.createOscillator();
+
+		// oscillator.frequency.value = notes[index][1];f
+
+		// let steps = +(document.querySelector('.sentezator > .settings > .set-steps > input').value);
+
+		// let imag = new global.Float32Array(steps);
+		// let real = new global.Float32Array(steps);
+		
+		// for (var i = 1; i < steps; i++) {
+		// 	imag[i] = 1 / (i * Math.PI);
+		// }
+
+		oscillator5.type = 'sawtooth'
+
+		// const wave = audioContext.createPeriodicWave(real, imag);
+
+		// oscillator.setPeriodicWave(wave);
+		oscillator5.frequency.setValueAtTime(((keys[index][1] + +(document.querySelectorAll('.set-osc')[4].children[2].value))), audioContext.currentTime);
+		oscillator5.connect(output);
+		oscillator5.start();
+	}
+
+	{
+		let oscillator6 = audioContext.createOscillator();
+
+		// oscillator.frequency.value = notes[index][1];f
+
+		// let steps = +(document.querySelector('.sentezator > .settings > .set-steps > input').value);
+
+		// let imag = new global.Float32Array(steps);
+		// let real = new global.Float32Array(steps);
+		
+		// for (var i = 1; i < steps; i++) {
+		// 	imag[i] = 1 / (i * Math.PI);
+		// }
+
+		oscillator6.type = 'sawtooth'
+
+		// const wave = audioContext.createPeriodicWave(real, imag);
+
+		// oscillator.setPeriodicWave(wave);
+		oscillator6.frequency.setValueAtTime(((keys[index][1] + +(document.querySelectorAll('.set-osc')[5].children[2].value))), audioContext.currentTime);
+		oscillator6.connect(output);
+		oscillator6.start();
+	}
 
 		function release(){
 			const interg = setInterval(() => {
-				volume.gain.value -= (0.008 * (volumeOfAllNotesT / 100))
+				volume.gain.value = 0
 				if (volume.gain.value <= 0){
+					volume.gain.value = 0;
 					let blackKeys = document.querySelectorAll('.keys > .black-keys > button')
 					let whiteKeys = document.querySelectorAll('.keys > .white-keys > button')
 					let elems = [...whiteKeys,...blackKeys]
@@ -227,38 +362,46 @@ function App() {
 					}
 					clearInterval(interg)
 				}
-			},6)
+			},(+(document.querySelectorAll('.set-ADSR > input')[4].value)))
 		}
 
+		let susteinTime = (+(document.querySelectorAll('.set-ADSR > input')[3].value))
 		function sustein(){
 			setTimeout(() => {
 				release()
-			}, 1200)
+			}, susteinTime)
 		}
 
+		let susteinLevel = (+(document.querySelectorAll('.set-ADSR > input')[2].value))
 		function decay(){
 			const interg = setInterval(() => {
-				volume.gain.value -= (0.08 * (volumeOfAllNotesT / 100))
-				if (volume.gain.value <= (0.4 * (volumeOfAllNotesT / 100))){
-					sustein()
-					clearInterval(interg)
-				}
-			},2)
+				volume.gain.value = ((susteinLevel / 1000) * (volumeOfAllNotesT / 100))
+				// if (volume.gain.value <= ((susteinLevel / 1000) * (volumeOfAllNotesT / 100))){
+				sustein()
+				clearInterval(interg)
+				// }
+			},(+(document.querySelectorAll('.set-ADSR > input')[1].value)))
 		}
 
+		let attackTime = (+(document.querySelectorAll('.set-ADSR > input')[0].value))
 		const interd = setInterval(() => {
-			volume.gain.value += (0 * (volumeOfAllNotesT / 100))
+			if (attackTime > 100){
+				volume.gain.value += (1 * (volumeOfAllNotesT / 100))
+			}else{
+				volume.gain.value += (1 * (volumeOfAllNotesT / 100))
+			}
 			if (volume.gain.value >= (1 * (volumeOfAllNotesT / 100))){
 				clearInterval(interd)
 				decay()
 			}
-		},0)
+		},attackTime)
 
 		playesNotes.push({
 			"audi":audioContext,
 			"index":index,
 			"whatkey":keys[index][2],
-			"isUp": false
+			"isUp": false,
+			"mainInterval":interd
 		})
 	}
 
@@ -298,6 +441,7 @@ function App() {
 					if (playesNotes[i].whatkey === ev.key){
 						playesNotes[i].isUp = true
 						playesNotes[i].audi.close()
+						clearInterval(playesNotes[i].mainInterval)
 						playesNotes = playesNotes.filter(n => n !== playesNotes[i])
 					}
 				}
@@ -376,6 +520,45 @@ return (
 					<div className='set-volume'>
 						<input type='range' onChange={e => setVolume(e.target.value)} min="1" max="100" value={volumeOfAllNotes}></input><span>Volume: {volumeOfAllNotes}</span>
 					</div>
+					<div className='set-ADSR'>
+						<h4>ADSR</h4>
+						<input type='range' onChange={e => setAttack(e.target.value)} min="1" max="1000" value={attack}></input><span>Attack: {attack}</span><br></br>
+
+						<input type='range' onChange={e => setDecay(e.target.value)} min="1" max="3000" value={decay}></input><span>Decay: {decay}</span><br></br>
+
+						<input type='range' onChange={e => setSusteinLevel(e.target.value)} min="1" max="1000" value={susteinLevel}></input><span>Sustein level: {susteinLevel}</span><br></br>
+
+						<input type='range' onChange={e => setSusteinTime(e.target.value)} min="1" max="10000" value={susteinTime}></input><span>Sustein time: {susteinTime}</span><br></br>
+
+						<input type='range' onChange={e => setRelay(e.target.value)} min="1" max="1000" value={relay}></input><span>Relay: {relay}</span><br></br>
+					</div>
+					<div className='oscils'>
+						<div className='set-osc'>
+							<h4>OSC 1</h4>
+							<span>bias:</span><input type='number' min="-1000" max="1000" value={bias1} onChange={e => {setBias1(e.target.value)}} step={1}></input><br></br>
+						</div>
+						<div className='set-osc'>
+							<h4>OSC 2</h4>
+							<span>bias:</span><input type='number' min="-1000" max="1000" value={bias2} onChange={e => {setBias2(e.target.value)}} step={1}></input><br></br>
+						</div>
+						<div className='set-osc'>
+							<h4>OSC 3</h4>
+							<span>bias:</span><input type='number' min="-1000" max="1000" value={bias3} onChange={e => {setBias3(e.target.value)}} step={1}></input><br></br>
+						</div>
+						<div className='set-osc'>
+							<h4>OSC 4</h4>
+							<span>bias:</span><input type='number' min="-1000" max="1000" value={bias4} onChange={e => {setBias4(e.target.value)}} step={1}></input><br></br>
+						</div>
+						<div className='set-osc'>
+							<h4>OSC 5</h4>
+							<span>bias:</span><input type='number' min="-1000" max="1000" value={bias5} onChange={e => {setBias5(e.target.value)}} step={1}></input><br></br>
+						</div>
+						<div className='set-osc'>
+							<h4>OSC 6</h4>
+							<span>bias:</span><input type='number' min="-1000" max="1000" value={bias6} onChange={e => {setBias6(e.target.value)}} step={1}></input><br></br>
+						</div>
+					</div>
+
 				</div>
 			</div>
     </div>
